eacf21190d30bf53ebc7d8d289f9d68bf3ad7166
yuzhihui <844370396@qq.com>
2021-06-17 20:12:14 +0800

    eureka

--- springCloud-provider-dept-8001/src/main/java/com/yuzh/springCloud/DeptApplication.java
+++ springCloud-provider-dept-8001/src/main/java/com/yuzh/springCloud/DeptApplication.java
@@ -3,11 +3,13 @@ package com.yuzh.springCloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.yuzh.springCloud.mapper")
 @EnableEurekaClient//auto register after the service start
+@EnableDiscoveryClient
 public class DeptApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeptApplication.class, args);
--- springCloud-provider-dept-8001/src/main/java/com/yuzh/springCloud/controller/DeptController.java
+++ springCloud-provider-dept-8001/src/main/java/com/yuzh/springCloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.yuzh.springCloud.controller;
 import com.yuzh.springCloud.pojo.Dept;
 import com.yuzh.springCloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +14,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/addDept")
     public boolean addDept(@RequestBody Dept dept) {
@@ -26,5 +31,23 @@ public class DeptController {
     @GetMapping("/queryAll")
     public List<Dept> queryAll() {
         return deptService.queryAll();
+    }
+
+    //
+    @GetMapping("/discover")
+    public Object discover() {
+        List<String> services = client.getServices();
+        System.out.println("discover=>service:" + services);
+
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost() + "\t" +
+                    instance.getPort() + "\t" +
+                    instance.getUri()+"\t"+
+                    instance.getServiceId()
+            );
+        }
+        return this.client;
     }
 }
